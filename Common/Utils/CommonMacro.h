3a13347e24c3435c2be49feedcbc7b0b95b9947c
COMPUTER\USER <gjduddnr5923@naver.com>
2024-09-13 13:56:39 +0900

    common project 에 기본적인 define, 함수들 추가.

---
+++ Common/Utils/CommonMacro.h
@@ -0,0 +1,72 @@
+#pragma once
+
+using namespace std;
+
+#define FIX_IN_RANGE(minVal, x, maxVal)			min(max(minVal,x), maxVal)
+#define IS_IN_RANGE(minVal, x, maxVal)			(x == FIX_IN_RANGE(minVal, x, maxVal)) ? true : false
+#define SAFE_DELETE(ptr)					\
+{											\
+	if(ptr != nullptr) {					\
+		delete ptr;							\
+	}										\
+	ptr = nullptr;							\
+}											\
+
+
+#define SAFE_DELETE_ARR(arr)				\
+{											\
+	if(arr != nullptr) {					\
+		delete[] arr;						\
+	}										\
+	arr = nullptr;							\
+}											\
+
+#define SAFE_RELEASE(ptr)					\
+{											\
+	if(ptr != nullptr) {					\
+		ptr->release();						\
+	}										\
+	ptr = nullptr;							\
+}											\
+
+#define SAFE_FREE(ptr)						\
+{											\
+	if(ptr != nullptr) {					\
+		free(ptr);							\
+	}										\
+	ptr = nullptr;							\
+}											\
+
+
+
+inline bool checkOverflow(UInt32 x, UInt32 add) {
+	auto orig = x;
+	auto ret = x + add;
+	return ((x & 0x80000000) != (ret & 0x80000000));
+}
+
+
+inline bool checkOverflow(UInt16 x, UInt16 add) {
+	auto orig = x;
+	auto ret = x + add;
+	return ((x & 0x8000) != (ret & 0x8000));
+}
+
+inline bool checkOverflow(UInt8 x, UInt8 add) {
+	auto orig = x;
+	auto ret = x + add;
+	return (( x & 0x80 ) != ( ret & 0x80 ));
+}
+
+template<typename T>
+UInt32 BufLen(T arr[]) {
+	return sizeof(arr) / sizeof(T);
+}
+
+#ifndef OUT
+#define OUT
+#endif
+
+#ifndef IN
+#define IN
+#endif
---
+++ Common/Utils/Types.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+using Int8 = __int8;
+using Int16 = __int16;
+using In32 = __int32;
+using Int64 = __int64;
+
+using UInt8 = unsigned __int8;
+using UInt16 = unsigned  __int16;
+using UInt32 = unsigned __int32;
+using UInt64 = unsigned __int64;
+
+using str_t = std::string;
+using Byte = char;
