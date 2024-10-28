93db28ef4860617bc1b05e4fe66a425081c83635
COMPUTER\USER <gjduddnr5923@naver.com>
2024-10-28 18:12:09 +0900

    test 코드 추가

--- Common/DB/DBManager.cpp
+++ Common/DB/DBManager.cpp
@@ -2,13 +2,39 @@
 #include "DBManager.h"
 
 void DBManager::Test()
-{/*
-	mysqlx::Session session(mysqlx::SessionOption::HOST, "localhost"
-		, mysqlx::SessionOption::PORT, 33306
-		, mysqlx::SessionOption::USER, "gameuser"
-		, mysqlx::SessionOption::PWD, "gameuser"
-	);
+{
+    try {
+        // MySQL 드라이버 인스턴스를 가져옵니다.
+        sql::Driver* driver = get_driver_instance();
 
-	mysqlx::Schema myDb = session.getSchema("testDB");
-	mysqlx::Table mytable = myDb.getTable("testTable");*/
+        // 데이터베이스에 연결합니다.
+        const string server = "tcp://127.0.0.1:3306";
+        const string name = "root";
+        const string password = "";
+        std::unique_ptr<sql::Connection> conn(driver->connect(server, name, password));
+
+        // 데이터베이스 작업을 수행합니다.
+        conn->setSchema("testdb");
+
+        // 쿼리 실행
+        std::unique_ptr<sql::Statement> stmt(conn->createStatement());
+        std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT * FROM user"));
+
+        // 결과 처리
+        while (res && res->next()) {
+            std::cout << res->getString(1) << " " << res->getString(2) << std::endl;
+        }
+
+    }
+    catch (sql::SQLException& e) {
+        std::cerr << "SQLException: " << e.what() << std::endl;
+        std::cerr << "Error code: " << e.getErrorCode() << std::endl;
+        std::cerr << "SQL state: " << e.getSQLState() << std::endl;
+    }
+    catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Unknown exception occurred" << std::endl;
+    }
 }
--- Common/DB/DBManager.h
+++ Common/DB/DBManager.h
@@ -1,6 +1,10 @@
 ﻿#pragma once
 //#include <mysqlx/xdevapi.h>
 
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+
 class DBManager : public Singleton<DBManager>
 {
 public:
