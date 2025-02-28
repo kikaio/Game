d8b60637346f1d3256960ab4d56a28b765390be3
COMPUTER\USER <gjduddnr5923@naver.com>
2025-02-28 15:54:02 +0900

    bat 파일 경로 부분 절대경로에서 상대경로로 수정.

--- launchService.bat
+++ launchService.bat
@@ -1,14 +1,14 @@
 echo "master server try start"
-cd C:\Users\user\Documents\Projects\portfolio\Game\Debug\MasterServer
+cd ./Debug/MasterServer
 start ./MasterServer.exe
 timeout /t 2
 
 echo "game server try start"
-cd C:\Users\user\Documents\Projects\portfolio\Game\Debug\Server
+cd ../Server
 start .\Server.exe
 timeout /t 2
 
 echo "client server try start"
-cd C:\Users\user\Documents\Projects\portfolio\Game\Debug\Client
+cd ../Client
 start .\Client.exe
 
