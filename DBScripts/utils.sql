c80ce2a3d83b2c2cd68dbb279f0b26b554e25a05
COMPUTER\USER <gjduddnr5923@naver.com>
2025-03-18 19:18:10 +0900

    utils성 쿼리 추가.

---
+++ DBScripts/utils.sql
@@ -0,0 +1,11 @@
+use commonDB;
+truncate table `profiles`;
+truncate table `platforms`;
+truncate table `accounts`;
+truncate table `summaries`;
+
+use gameDB;
+truncate table `items`;
+truncate table `costumes`;
+truncate table `characters`;
+truncate table `gameusers`;
